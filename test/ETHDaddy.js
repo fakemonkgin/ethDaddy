4c5790c9f59b2f1118d17a7e889105c806c0668d
fakemonkgin <ginli2013@gmail.com>
2022-12-21 09:33:38 +0800

    initial

---
+++ test/ETHDaddy.js
@@ -0,0 +1,14 @@
+const { expect } = require("chai")
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+describe("ETHDaddy", () => {
+    it("has a name", async() => {
+      const ETHDaddy = await ethers.getContractFactory("ETHDaddy");
+      let ethDaddy = await ETHDaddy.deploy();
+      const result = await ethDaddy.name();
+      expect(result).to.equal("ETHDaddy");
+    })
+})
